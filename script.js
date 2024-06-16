e50fb6bf847db37295900397507528623562a309
Siddikpatel <patel.siddik369@gmail.com>
2024-06-16 22:19:11 +0530

    2.0.9 copyright year is real-time

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const slider = document.querySelector('.slider');
-const img = document.querySelectorAll('.s-img');
+// const left = document.querySelector('.left');
+// const right = document.querySelector('.right');
+// const slider = document.querySelector('.slider');
+// const img = document.querySelectorAll('.s-img');
 
-let slideNum = 1;
-const len = img.length;
+// let slideNum = 1;
+// const len = img.length;
 
-const nextSlide = () => {
-    if(slideNum < len) {
-        slider.style.transform = `translateX(-${slideNum * 1000}px)`;
-        slideNum++;
-    }
-}
+// const nextSlide = () => {
+//     if(slideNum < len) {
+//         slider.style.transform = `translateX(-${slideNum * 1000}px)`;
+//         slideNum++;
+//     }
+// }
 
-const prevSlide = () => {
-    if(slideNum > 1) {
-        slider.style.transform = `translateX(-${(slideNum - 2) * 1000}px)`;
-        slideNum--;
-    }
-}
+// const prevSlide = () => {
+//     if(slideNum > 1) {
+//         slider.style.transform = `translateX(-${(slideNum - 2) * 1000}px)`;
+//         slideNum--;
+//     }
+// }
 
-right.addEventListener('click', () => {
-    nextSlide();
-});
+// right.addEventListener('click', () => {
+//     nextSlide();
+// });
 
-left.addEventListener('click', () => {
-    prevSlide();
-});
+// left.addEventListener('click', () => {
+//     prevSlide();
+// });
+
+const year = document.getElementById("year");
+const currentYear = new Date().getFullYear();
+year.setAttribute("datetime", currentYear);
+year.textContent = currentYear;
