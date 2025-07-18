e66230a12bbf23b3f3a68fa073eabe6ad569bf85
Siddikpatel <patel.siddik369@gmail.com>
2025-07-18 19:53:42 -0300

    Added Dependency Visualizer's link

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// const left = document.querySelector('.left');
-// const right = document.querySelector('.right');
-// const slider = document.querySelector('.slider');
-// const img = document.querySelectorAll('.s-img');
-
-// let slideNum = 1;
-// const len = img.length;
-
-// const nextSlide = () => {
-//     if(slideNum < len) {
-//         slider.style.transform = `translateX(-${slideNum * 1000}px)`;
-//         slideNum++;
-//     }
-// }
-
-// const prevSlide = () => {
-//     if(slideNum > 1) {
-//         slider.style.transform = `translateX(-${(slideNum - 2) * 1000}px)`;
-//         slideNum--;
-//     }
-// }
-
-// right.addEventListener('click', () => {
-//     nextSlide();
-// });
-
-// left.addEventListener('click', () => {
-//     prevSlide();
-// });
-
 const year = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 year.setAttribute("datetime", currentYear);
